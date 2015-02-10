dd2003dbd3077aaf4de0a555c20bbc842970749c
audiua <audiua@yandex.ru>
2015-02-10 13:06:40 +0200

    vk twitter

--- themes/bootstrap/views/layouts/sidebar.php
+++ themes/bootstrap/views/layouts/sidebar.php
@@ -1,6 +1,5 @@
 <div class="sidebar">
 
-
 <div class="sidebar-block">
 	<!-- <div class="block-title">Block title</div> -->
 	<noindex>
@@ -40,12 +39,50 @@
 
 </div>
 
+
 <div class="sidebar-block">
 	<div class="lead text-center">Навігація по сайту</div>
 	<?php $this->widget('TreeMenuWidget'); ?>
 </div>
 
 
+<div class="sidebar-block">
+	<div class="lead text-center">Соц. мережі</div>
+	<noindex>
+		<div role="tabpanel">
+
+		  <!-- Nav tabs -->
+		  <ul class="nav nav-tabs" role="tablist">
+		    <li role="presentation" class="active"><a href="#vk" aria-controls="vk" role="tab" data-toggle="tab">vk</a></li>
+		    <li role="presentation"><a href="#tw" aria-controls="tw" role="tab" data-toggle="tab">tw</a></li>
+		  </ul>
+
+		  <!-- Tab panes -->
+		  <div class="tab-content">
+		    <div role="tabpanel" class="tab-pane active" id="vk">
+				<script type="text/javascript" src="//vk.com/js/api/openapi.js?115"></script>
+
+				<!-- VK Widget -->
+				<div id="vk_groups"></div>
+				<script type="text/javascript">
+					VK.Widgets.Group("vk_groups", {mode: 0, width: "300", height: "150", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 86651632);
+				</script>
+		    </div>
+
+		    <div role="tabpanel" class="tab-pane" id="tw">
+	   
+				<a rel="nofollow" class="twitter-timeline"  href="https://twitter.com/mygdz" data-widget-id="565102958497763328">Твиты от @mygdz</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				
+
+		    </div>
+
+		  </div>
+
+		</div>
+	</noindex>
+
+</div>
 
 
 </div>
