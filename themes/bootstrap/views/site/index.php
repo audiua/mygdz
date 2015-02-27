429fb97a84536c6b23c23c4e284f1ceb8298db2a
audiua <audiua@yandex.ru>
2015-02-27 23:13:21 +0200

    full banner

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -27,6 +27,17 @@
 </div>
 
 <div class="clear"></div>
+<div class="full-banner">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- my полный баннер -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-9657826060070920"
+	     data-ad-slot="2417505095"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</div>
 
 <div class="all-book">
 	<p class="lead">Останні надходження: </p>
--- themes/bootstrap/views/site/subject.php
+++ themes/bootstrap/views/site/subject.php
@@ -6,6 +6,19 @@
 
 <div class="clear"></div>
 
+<div class="full-banner">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- my полный баннер -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-9657826060070920"
+	     data-ad-slot="2417505095"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</div>
+
+
 <div class="all-book">
 	<?php $this->widget('DataBookWidget', array('model'=>$books)); ?>
 </div>
