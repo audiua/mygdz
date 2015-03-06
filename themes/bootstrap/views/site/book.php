df728d0f65a205bb66d62c58cff3d764106628d5
audiua <audiua@yandex.ru>
2015-03-06 21:49:30 +0200

    add top banner

--- themes/bootstrap/views/site/book.php
+++ themes/bootstrap/views/site/book.php
@@ -1,3 +1,16 @@
+<div class="full-banner">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- my full ban top -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-9657826060070920"
+	     data-ad-slot="9080670692"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</div>
+
+
 <h1><?= $this->h1; ?></h1>
 
 <?php $this->widget('OneBookWidget'); ?>
