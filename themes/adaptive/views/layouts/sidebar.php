8c7e731e21882d8a5da5aac53ff8afeb750b815b
audiua <audiua@yandex.ru>
2015-09-28 17:11:46 +0300

    add banner

--- themes/adaptive/views/layouts/sidebar.php
+++ themes/adaptive/views/layouts/sidebar.php
@@ -1,4 +1,16 @@
 <div class="sidebar">
+	<div class="banner">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- my небоскреб -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-9657826060070920"
+     data-ad-slot="9475257091"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+	</div>
+
 	<div class="row">
 		<div class="col-md-12 col-lg-12 tree">
 			<div class="lead">Навігація по сайту</div>
