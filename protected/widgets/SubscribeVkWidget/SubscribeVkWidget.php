68865cb780f1a652f727f8c135a02e907121ab32
audiua <audiua@yandex.ru>
2015-11-08 13:18:36 +0200

    fix vk

---
+++ protected/widgets/SubscribeVkWidget/SubscribeVkWidget.php
@@ -0,0 +1,14 @@
+<?php
+
+class SubscribeVkWidget extends CWidget{
+
+	public function init(){
+        parent::init();
+    }
+
+	public function run(){
+        $this->render('index');
+    }
+
+    
+}
---
+++ protected/widgets/SubscribeVkWidget/views/index.php
@@ -0,0 +1,24 @@
+	<!-- Small modal -->
+	<div class="modal fade" id="fb-modal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	    	<div class="modal-header">
+	    		<div class="modal-title" id="myModalLabel">Підписуйся на публічну сторінку <span class="shkolyar blue">mygdz.pp.ua</span> в <span class="shkolyar blue">Vkontakte</span></div>
+	      	</div>
+
+			<!-- VK Widget -->
+			<div id="vk_groups2"></div>
+			<script type="text/javascript">
+			VK.Widgets.Group("vk_groups2", {mode: 0, width: "300", height: "320", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 86651632);
+			</script>
+
+
+
+			<div class="clearfix"></div>
+
+	    	<div class="modal-footer">
+	    		<button type="button" class="btn btn-default" data-dismiss="modal">Мені вже подобається <span>mygdz.pp.ua</span></button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
