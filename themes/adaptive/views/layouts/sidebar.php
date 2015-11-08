9181e994fc766ac063a8cf2dd19faaba71a88669
audiua <audiua@yandex.ru>
2015-11-08 13:11:32 +0200

    fix vk

--- themes/adaptive/views/layouts/sidebar.php
+++ themes/adaptive/views/layouts/sidebar.php
@@ -39,7 +39,7 @@
 						<!-- VK Widget -->
 						<div id="vk_groups"></div>
 						<script type="text/javascript">
-							VK.Widgets.Group("vk_groups", {mode: 0, width: "280", height: "320", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 86651632);
+							VK.Widgets.Group("vk_groups", {mode: 0, width: "280", height: "80", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 86651632);
 						</script>
 				    </div>
 
