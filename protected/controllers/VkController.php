5d99ee05b349bf8ae03d3e1a95d5ec0d82d93345
audiua <audiua@yandex.ru>
2015-12-11 17:51:41 +0200

    vk posting

--- protected/controllers/VkController.php
+++ protected/controllers/VkController.php
@@ -3,36 +3,42 @@
 
 class VkController extends Controller{
 
-	const CLIENT_ID = '4745114';
-	const ACCESS_TOKEN = 'b7e28a389111ccb022425ef7db3ab2e7037c6ae2c507b21074899edf3e12b91ec0ae040ba7dc2ef8c1442';
+	const CLIENT_ID = '4774972';
+	const ACCESS_TOKEN = '47fdd92a4908b8e57213eb978c14488560494f0e4b6289e5f7ac7723308c8a06c23d8a7bba8666369f4f4';
 
 	// флаг что есть публ
 	private $_p=false;
 
 	public function actionIndex($hash=null){
 
-		if($hash !== '5b717f9e843de36448780e90f00942fc'){
-			Yii::app()->end();
-		}
-
+		// if($hash !== '5b717f9e843de36448780e90f00942fc'){
+		// 	Yii::app()->end();
+		// }
 		$model = $this->getGdz();
+		// $img =  $model->getThumb('origin');
+		// d();
 
-		// d($model);
+		Yii::import('ext.vkontakte-php-sdk-master.src.Vkontakte');
+		// require_once('../src/Vkontakte.php');
 
+		
+		
 		if($model){
 
 			$str = '';
 
 
-			if($this->_p){
-				$str .= $this->normalDate(time()).' обновлено гдз ';
-			} else {
-				$str .= $this->normalDate(strtotime($model->public_time)).' добавлено гдз ';
-			}	
+			// if($this->_p){
+			// 	$str .= $this->normalDate(time()).' обновлено гдз ';
+			// } else {
+			// }	
+			$str .= $this->normalDate(strtotime($model->publish_date)).' добавлено підручник ';
 
-			$str .= $model->clas->slug . ' клас ' . $model->subject->title . ' ' . $model->author  . ' ' . Yii::app()->createAbsoluteUrl( $model->getUrl() );
+			$str .= $model->clas->name . ' клас ' . $model->subject->name . ' ' . $model->author  . ' ' . $model->year  . 'рік ' . Yii::app()->createAbsoluteUrl( $model->getUrl() );
 
-			$this->invoke('wall.post', array(
+			$accessToken = self::ACCESS_TOKEN;
+			$vkAPI = new Vkontakte(['access_token' => $accessToken]);
+			$r = $vkAPI->api('wall.post',array(
 			    'owner_id' => -86651632,
 			    'message' => $str,
 			    'from_group' => 1
@@ -42,32 +48,29 @@ class VkController extends Controller{
 		Yii::app()->end();
 	}
 
-	private function invoke($name, array $params = array()){
-		$params['access_token'] = self::ACCESS_TOKEN;
-		$content = file_get_contents('https://api.vkontakte.ru/method/'.$name.'?'.http_build_query($params));
-		$result = json_decode($content);
-		print_r($result);
-	}
+	// https://api.vk.com/method/users.get?user_id=66748&v=5.40&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3
+
 
 	private function getGdz(){
 
 		$criteria = new CDbCriteria;
 		$criteria->condition = 'public=1';
-		$criteria->order = 'update_time ASC';
-		$gdz = Book::model()->find($criteria);
+		$criteria->addCondition('vk_publish_time=0');
+		$criteria->addCondition('publish_date<'.time());
+		$gdz = Textbook::model()->find($criteria);
 		// d($gdz);
 
-		if($gdz){
-			$this->_p = true;
-			$gdz->update_time = time();
-			if( ! $gdz->update()){
-				die($gdz->gerErrors);
-			}
+		return $gdz;
+		// if($gdz){
+		// 	$this->_p = true;
+		// 	$gdz->update_time = time();
+		// 	if( ! $gdz->update()){
+		// 		die($gdz->gerErrors);
+		// 	}
 		
-			return $gdz;
-		}
+		// }
 
-		return null;
+		// return null;
 
 	}
 
