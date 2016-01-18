e485dbb3d7bc7f0c9bb2ddda23375451900da8e3
audiua <audiua@yandex.ru>
2016-01-18 21:10:09 +0200

    fix

--- themes/adaptive/views/site/book.php
+++ themes/adaptive/views/site/book.php
@@ -5,16 +5,6 @@
 <div class="separator"></div>
 <div class="clearfix"></div>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- my_above_task_big -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
-     data-ad-client="ca-pub-9657826060070920"
-     data-ad-slot="6880487499"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
 <div class="task">
 	<!-- <section id="inverted-contain">
 		<div class="loading"></div>
