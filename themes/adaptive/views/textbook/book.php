03b505fbaa067f0ecbfb7f163437894e278e6f65
Kostia Tarnashynskyi <kostia.tarnashynskyi@gmail.com>
2017-02-11 18:06:28 +0200

    ads

--- themes/adaptive/views/textbook/book.php
+++ themes/adaptive/views/textbook/book.php
@@ -5,17 +5,38 @@
 <div class="separator"></div>
 <div class="clearfix"></div>
 
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- my_mobi_middle -->
+<ins class="adsbygoogle"
+	 style="display:inline-block;width:336px;height:280px"
+	 data-ad-client="ca-pub-9657826060070920"
+	 data-ad-slot="9749047893"></ins>
+<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- my_mobi_middle -->
+<ins class="adsbygoogle"
+	 style="display:inline-block;width:336px;height:280px"
+	 data-ad-client="ca-pub-9657826060070920"
+	 data-ad-slot="9749047893"></ins>
+<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<div class="separator"></div>
+<div class="clearfix"></div>
 <?php if($embedConfigId): ?>
 <iframe width="700" height="500" src="//e.issuu.com/embed.html#<?= $embedConfigId; ?>" frameborder="0" allowfullscreen></iframe>
 <?php endif; ?>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- my_adaptive_top -->
+<!-- my_netboard_middle -->
 <ins class="adsbygoogle"
-	 style="display:block"
+	 style="display:inline-block;width:580px;height:400px"
 	 data-ad-client="ca-pub-9657826060070920"
-	 data-ad-slot="5318848293"
-	 data-ad-format="auto"></ins>
+	 data-ad-slot="2539516294"></ins>
 <script>
 	(adsbygoogle = window.adsbygoogle || []).push({});
 </script>
