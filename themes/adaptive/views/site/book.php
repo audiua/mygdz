93c01f71dd2a768b919d629b4dc2fb508206fc1b
Kostya <audiua@yandex.ru>
2017-10-04 22:54:35 +0300

    add relevant pages

--- themes/adaptive/views/site/book.php
+++ themes/adaptive/views/site/book.php
@@ -78,6 +78,17 @@
 <div class="separator"></div>
 
 <div class="lead">Схожі збірники гдз: </div>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-9657826060070920"
+     data-ad-slot="1342143091"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 <div class="book-list">
 	<?php $this->widget('RelativeBooksWidget', array('mode'=>'clas')); ?>
 </div>
